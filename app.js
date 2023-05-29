4e1fb61f05f2fbea73e63c53c3478a6ead4cac2c
anonymousearly <elaco.earl13@gmail.com>
2023-05-29 08:59:21 +0800

    working, thanks to Rairu

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-function loginB(){
-    var username = document.querySelector('#userid').value;
-    var password = document.querySelector('#passid').value;
-    var key = JSON.parse(user);
-    var user = localStorage.getItem('user');
+function registrationB(){
+    let username = document.querySelector('#reguser');
+    let password = document.querySelector('#regpass');
+    let email = document.querySelector('#regemail');
 
+    if(username.value == '' || password.value == '' || email.value == ''){
+        alert('Please input what is required!');
+    } else {
+        let user = {
+            username: username.value,
+            password: password.value,
+            email: email.value
+        }
+
+        let json = JSON.stringify(user);
+
+        localStorage.setItem('user', json);
+        console.log('User Added');
+        alert('You are now registered! Click ok to continue');
+        username.value = '';
+        password.value = '';
+        email.value = '';
+    }
+}
+
+function loginB(){
+    let username = document.querySelector('#getuser').value;
+    let password = document.querySelector('#getpass').value;
+    let user = localStorage.getItem('user');
+    let key = JSON.parse(user);
 
     if(username == '' && password == ''){
         alert('Please input what is required!');
@@ -13,30 +37,5 @@ function loginB(){
         return false;
     } else {
         alert('User Not Found!');
-    }
-}
-
-function registrationB(){
-    var username = document.querySelector('#reguser');
-    var password = document.querySelector('#regpass');
-    var email = document.querySelector('#regemail');
-
-    if(username.value == '' || password.value == '' || email.value == ''){
-        alert('Please input what is required!');
-    } else {
-        var user = {
-            username: username.value,
-            password: password.value,
-            email: email.value
-        }
-
-        var json = JSON.stringify(user);
-
-        localStorage.setItem('user', json);
-        console.log('User Added');
-        alert('You are now registered! Click ok to continue');
-        username.value = '';
-        password.value = '';
-        email.value = '';
     }
 }
