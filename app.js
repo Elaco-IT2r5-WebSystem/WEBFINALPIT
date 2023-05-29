cd66192430e8643bd6440c48ab376739f850193d
anonymousearly <elaco.earl13@gmail.com>
2023-05-29 09:09:35 +0800

    lezgoo

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 function registrationB(){
-    let username = document.querySelector('#reguser');
-    let password = document.querySelector('#regpass');
-    let email = document.querySelector('#regemail');
+    var username = document.querySelector('#reguser');
+    var password = document.querySelector('#regpass');
+    var email = document.querySelector('#regemail');
 
     if(username.value == '' || password.value == '' || email.value == ''){
         alert('Please input what is required!');
     } else {
-        let user = {
+        var user = {
             username: username.value,
             password: password.value,
             email: email.value
         }
 
-        let json = JSON.stringify(user);
+        var json = JSON.stringify(user);
 
         localStorage.setItem('user', json);
         console.log('User Added');
@@ -24,10 +24,10 @@ function registrationB(){
 }
 
 function loginB(){
-    let username = document.querySelector('#getuser').value;
-    let password = document.querySelector('#getpass').value;
-    let user = localStorage.getItem('user');
-    let key = JSON.parse(user);
+    var username = document.querySelector('#getuser').value;
+    var password = document.querySelector('#getpass').value;
+    var user = localStorage.getItem('user');
+    var key = JSON.parse(user);
 
     if(username == '' && password == ''){
         alert('Please input what is required!');
